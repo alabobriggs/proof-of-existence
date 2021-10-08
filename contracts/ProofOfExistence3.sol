0e1cadc1201b3f17e12ce79ed9f2132936aaba0d
alabobriggs <alabobriggs@gmail.com>
2021-10-09 00:44:49 +0100

    fix artifacts

--- contracts/ProofOfExistence3.sol
+++ contracts/ProofOfExistence3.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract ProofOfExistence2 {
+contract ProofOfExistence3 {
     mapping(bytes32 => bool) private proofs;
 
     // store a proof of existence in the contract state
